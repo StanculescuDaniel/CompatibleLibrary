fb0d18f738c72d91646ae422030d1ea24be166da
daniel <danie@DESKTOP-69EOMS5>
2020-10-29 16:57:41 +0200

    Clean global.asax

--- WebApplicationNetFram48/Global.asax.cs
+++ WebApplicationNetFram48/Global.asax.cs
@@ -3,11 +3,9 @@ using Autofac.Extensions.DependencyInjection;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
 using CompatibleLibrary;
-using CompatibleLibrary.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace WebApplicationNetFram48
 {
@@ -18,25 +16,16 @@ namespace WebApplicationNetFram48
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            //RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-
+            ContainerBuilder builder = new ContainerBuilder();
             IServiceCollection services = new ServiceCollection();
             services.AddCountriesClient();
-            var builder = new ContainerBuilder();
+            
             builder.Populate(services);
-
-
-
             builder.RegisterControllers(typeof(WebApiApplication).Assembly);
             builder.RegisterApiControllers(typeof(WebApiApplication).Assembly);
-            
-            var container = builder.Build();
 
-            using(var scope = container.BeginLifetimeScope())
-            {
-                var x = scope.Resolve<ICountriesServiceClient>();
-            }
+            IContainer container = builder.Build();
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
 
